5636eca5b37d051dd6804737c7bd54da2cbbe5d7
WIDE888 <38092951+WIDE888@users.noreply.github.com>
2018-04-06 02:00:32 +0900

    Update yescrypt.c

--- yescrypt.c
+++ yescrypt.c
@@ -22,8 +22,8 @@
 #include "yescrypt-best.c"
 
 #define YESCRYPT_N 4096
-#define YESCRYPT_R 16
-#define YESCRYPT_P 4
+#define YESCRYPT_R 32
+#define YESCRYPT_P 1
 #define YESCRYPT_T 0
 #define YESCRYPT_FLAGS (YESCRYPT_RW | YESCRYPT_PWXFORM)
 
